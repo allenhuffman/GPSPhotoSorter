2c65bd97db239b1fe1e06652ee8381d3691c452b
Allen Huffman <alsplace@pobox.com>
2023-03-10 22:46:33 -0600

    Added command line options and status output
    Added command line options for specifying input directory (photos), output directory, and directory to copy photos without GPS data. Also made it print what it was moving and to where.

--- sortphotosbygps.py
+++ sortphotosbygps.py
@@ -1,3 +1,4 @@
+#!/usr/bin python3
 #
 # GPS Photo Sorter
 # Created (mostly) by chat.chatgpt.com
@@ -10,9 +11,24 @@ import xml.etree.ElementTree as ET
 import exifread
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
+import argparse
+
+parser = argparse.ArgumentParser(description='Sort photos based on GPS data.')
+parser.add_argument('--kml', type=str, default='GPS Areas.kml',
+                    help='Path to KML file containing GPS areas (default: GPS Areas.kml)')
+parser.add_argument('--input', type=str, default='photos',
+                    help='Path to directory containing photos (default: photos)')
+parser.add_argument('--output', type=str, default='sorted_photos',
+                    help='Path to directory where sorted photos will be moved (default: sorted_photos)')
+parser.add_argument('--nogpsdir', type=str, default='photos_no_gps',
+                    help='Path to directory where photos without GPS data be moved (default: photos_no_gps)')
+
+args = parser.parse_args()
+
+# Use args.kml, args.input, args.output and args.nogpsdir in your script
 
 # Parse the KML file and extract the coordinates of the Placemarks
-tree = ET.parse('GPS Areas.kml')
+tree = ET.parse(args.kml)
 root = tree.getroot()
 placemarks = {}
 for pm in root.iter('{http://www.opengis.net/kml/2.2}Placemark'):
@@ -21,16 +37,16 @@ for pm in root.iter('{http://www.opengis.net/kml/2.2}Placemark'):
     coords = [(float(c.split(',')[0]), float(c.split(',')[1])) for c in coords_str.split()]
     placemarks[name] = Polygon(coords)
     print('  Checking Placemark:', name)
-    print('    Placemark coordinates:', coords)
+    #print('    Placemark coordinates:', coords)
 
 # Loop through each photo and move it to the appropriate folder
-for filename in os.listdir('photos'):
+for filename in os.listdir(args.input):
     if filename.endswith('.jpg'):
-        with open(os.path.join('photos', filename), 'rb') as f:
+        with open(os.path.join(args.input, filename), 'rb') as f:
             tags = exifread.process_file(f)
             # for tag in tags.keys():
-            #     if 'GPS' in tag:
-            #         print(tag, tags[tag])
+            #      if 'GPS' in tag:
+            #          print(tag, tags[tag])
             try:
                 lat = float(tags['GPS GPSLatitude'].values[0].num) / float(tags['GPS GPSLatitude'].values[0].den) + \
                     (float(tags['GPS GPSLatitude'].values[1].num) / float(tags['GPS GPSLatitude'].values[1].den))/60 + \
@@ -46,18 +62,21 @@ for filename in os.listdir('photos'):
                 if (tags['GPS GPSLongitudeRef'].values == 'W'):
                     lon = -lon
 
-                print('  Photo coordinates:', lat, lon)
+                #print('  Photo coordinates:', lat, lon)
                 point = Point(lon, lat)
                 for name, polygon in placemarks.items():
                     if polygon.contains(point):
-                        dest_dir = os.path.join('photos', name)
+                        dest_dir = os.path.join(args.output, name)
                         if not os.path.exists(dest_dir):
                             os.makedirs(dest_dir)
-                        shutil.move(os.path.join('photos', filename), os.path.join(dest_dir, filename))
+                        print(f'Moving {filename} to {dest_dir}')
+                        shutil.move(os.path.join(args.input, filename), os.path.join(dest_dir, filename))
                         break
             except:
-                dest_dir = os.path.join('photos', 'NoGPS')
-                if not os.path.exists(dest_dir):
-                    os.makedirs(dest_dir)
-                shutil.move(os.path.join('photos', filename), os.path.join(dest_dir, filename))
+                if not os.path.exists(args.nogpsdir):
+                    os.makedirs(args.nogpsdir)
+                print(f'Moving {filename} to {args.nogpsdir}')
+                shutil.move(os.path.join(args.input, filename), os.path.join(args.nogpsdir, filename))
                 pass # ignore photos without GPS data or with invalid GPS data
+
+# End of script.
